50db314201b3b79e19905387b734ab47befc9c41
Ehtisham <ehtisham@gmail.com>
2022-10-21 11:30:06 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // * means we goona add tailwind css in all file
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
